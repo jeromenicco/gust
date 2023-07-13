c23759fd41a03409da0ccf374ea7d3de86549b52
Jerome Nicco <jeromenicco36@gmail.com>
2023-07-13 13:27:38 +0200

    first commit

---
+++ src/app/components/GustLogo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GustLogo() {
+  return (
+    <div className='logo__container'>
+      <div className='logo__wrapper'>
+        <h1>Gust<span>!</span></h1>
+      </div>
+      <h2>Coming soon...</h2>
+    </div>
+  )
+}
+
+export default GustLogo
